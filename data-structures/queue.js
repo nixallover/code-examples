65bd765dabcbd82d3173200b7a7139e7d7a86005
Nicole Oliver <nicole.oliver.42@gmail.com>
2017-03-19 09:00:22 -0700

    added priority queue modification

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -1,8 +1,35 @@
 function Queue(){
   var items = [];
 
-  this.enqueue = function(value){
-    items.push(value);
+  // for basic queue
+  // this.enqueue = function(value){
+  //   items.push(value);
+  // }
+
+  // for priority queue
+  function QueueElement(element, priority){
+    this.element = element;
+    this.priority = priority;
+  }
+
+  this.enqueue = function(element, priority){
+    var queueElement = new QueueElement(element, priority);
+
+    if(this.isEmpty()){
+      items.push(queueElement);
+    } else {
+      var added = false;
+      for(var i=0; i<items.length; i++){
+        if(queueElement.priority < items[i].priority){
+          items.splice(i, 0, queueElement);
+          added = true;
+          break;
+        }
+      }
+      if(!added){
+        items.push(queueElement);
+      }
+    }
   }
 
   this.dequeue = function(){
@@ -22,7 +49,15 @@ function Queue(){
   }
 
   this.print = function(){
-    console.log(items.toString());
+    // basic
+    //console.log(items.toString());
+
+    // priority
+    let myString = "";
+    for(var i=0; i<items.length; i++){
+      myString += items[i].element + ' ';
+    }
+    console.log(myString);
   }
 }
 
@@ -30,10 +65,21 @@ console.log('Queue implementation test');
 
 var queue = new Queue();
 
+// basic
+// console.log(queue.isEmpty());
+// queue.enqueue("Jack");
+// queue.enqueue("Alana");
+// queue.enqueue("Xena");
+// queue.print();
+// queue.dequeue();
+// queue.print();
+// console.log(queue.isEmpty());
+
+// priority
 console.log(queue.isEmpty());
-queue.enqueue("Jack");
-queue.enqueue("Alana");
-queue.enqueue("Xena");
+queue.enqueue("Jack", 1);
+queue.enqueue("Alana", 3);
+queue.enqueue("Xena", 2);
 queue.print();
 queue.dequeue();
 queue.print();
